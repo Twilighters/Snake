51ad3d3cc56b0b91cabcd09ab8bbbb93937edfe3
TwilighterS <ilya475@rambler.ru>
2020-03-15 15:05:24 +0300

    Mechanics of eating food by a snake

--- Program.cs
+++ Program.cs
@@ -26,9 +26,26 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            
+
+            //генерация точек соответствующих еде
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+
+
             while (true)
             {
+                if (snake.Eat(food))
+
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
